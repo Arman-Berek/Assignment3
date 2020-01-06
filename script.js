b8d4be4248df21af87ae0219526f8d87aa8a1907
Arman Berek <armanberek@MacBook-Air-10733.local>
2020-01-06 13:31:36 -0500

    finished 4,5

---
+++ script.js
@@ -0,0 +1,6 @@
+// 4 - Select a list item with a class of third, but only the list item inside of the ol tag.
+document.querySelector("ol .third");
+
+//5
+var changeContainer = document.querySelector("#container");
+changeContainer.innerText = "Hello!";
