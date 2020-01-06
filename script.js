5271f9ba4be2dc3e506f05c9f6e0d796c5637a59
Nursima Donuk <nursimadonuk@gmail.com>
2020-01-06 13:41:32 -0500

    added 123

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-// 4 - Select a list item with a class of third, but only the list item inside of the ol tag.
+//1) Select the section with an id of container without using querySelector.
+val cont = document.getElementById("container");
+
+//2) Select the section with an id of container using querySelector.
+val contWithQ = document.querySelector("container");
+
+//3) Select all of the list items with a class of "second".
+val sec = document.getElementsByClassName("second");
+
+
+// 4- Select a list item with a class of third, but only the list item inside of the ol tag.
 document.querySelector("ol .third");
 
 //5
