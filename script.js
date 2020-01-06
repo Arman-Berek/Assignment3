93f5dd3d3da755773ebe8f32717a5f10359feb31
Arman Berek <armanberek@MacBook-Air-10733.local>
2020-01-06 13:43:29 -0500

    finished 6,7

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // 4 - Select a list item with a class of third, but only the list item inside of the ol tag.
 document.querySelector("ol .third");
 
-//5
+//5 - Give the section with an id of container the text "Hello!".
 var changeContainer = document.querySelector("#container");
 changeContainer.innerText = "Hello!";
+
+//6 - Add the class main to the div with a class of footer.
+var getFooter = document.querySelector(".footer");
+getFooter.classList.add("main");
+
+//7 - Remove the class main on the div with a class of footer.
+var getFooterAgain = document.querySelector(".footer");
+getFooterAgain.classList.remove("main");
